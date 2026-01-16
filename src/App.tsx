f8c4840650fc02572e7f234c8bda225e021cc12b
y20-code <yls3235882107@gmail.com>
2026-01-16 22:56:08 +0800

    feat(storage):Implement the data persistence and reset functions of LocalStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import {useState, useEffect,useMemo} from 'react';
-import { Card,Input,Radio,Space } from 'antd';
+import { Card,Input,message,Radio,Space,Button } from 'antd';
 import { SearchOutlined } from '@ant-design/icons';
 import { generateWords } from './utils/mock';
 import {type WordItem } from './types';
@@ -8,7 +8,9 @@ import WordForm  from './component/WordForm';
 
 const App = () => {
 
-  const [words,setwords] = useState<WordItem[]>([])
+  const CACHE_KEY = 'word_killer_data';
+
+  const [words,setWords] = useState<WordItem[]>([])
 
   const [keyword,setKeyword] = useState('');
   const [filterLevel,setFilterLevel] = useState('all')
@@ -28,18 +30,51 @@ const App = () => {
   },[words,keyword,filterLevel])
 
   useEffect(() => {
-    const data = generateWords(10000);
-    setwords(data);
-    console.log("数据工厂产出:" ,data)
+    
+    const cache = localStorage.getItem(CACHE_KEY);
+
+    if(cache){
+      try{
+        const data = JSON.parse(cache);
+        setWords(data)
+        console.log('读取缓存成功')
+      } catch (error) {
+        console.error('缓存数据损坏，重新生成...', error);
+        const data = generateWords(20000);
+        setWords(data);
+      }
+    }else{
+      console.log('正在生成')
+      const data = generateWords(10000);
+      setWords(data);
+      console.log("数据工厂产出:" ,data)
+    }
   },[])
+
+  useEffect(() => {
+    if(words.length>0){
+      localStorage.setItem(CACHE_KEY,JSON.stringify(words))
+      // message.success("数据保存成功")
+    }
+  },[words])
+
+  const handleRemoveAll = () =>{
+    localStorage.removeItem(CACHE_KEY)
+    message.success('已清空缓存，系统重置中...');
+    
+    // 延迟一点点再刷新，让用户看清提示
+    setTimeout(() => {
+        window.location.reload();
+    }, 1000);
+  }
 
   const handleDelete = (id:string) =>{
       const newWords = words.filter(item => item.id !==id);
-      setwords(newWords);
+      setWords(newWords);
   }
 
   const handleAdd = (item:WordItem) => {
-    setwords(prev => [item,...prev])
+    setWords(prev => [item,...prev])
   }
 
 
@@ -78,6 +113,8 @@ const App = () => {
               <Radio.Button value="六级">六级</Radio.Button>
               <Radio.Button value="雅思">雅思</Radio.Button>
             </Radio.Group>
+
+            <Button onClick={handleRemoveAll}>重置数据</Button>
             </Space>
           </Card>
             
